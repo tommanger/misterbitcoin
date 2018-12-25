ab1eca2fbd6b9f85b32620fb81f6fe7d33167527
tom manger <tom.manger10@gmail.com>
2018-12-25 18:32:01 +0200

    Updates

---
+++ precache-manifest.dd84d57807c3e5a53c35f3aa055a6b55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cccbea502128d375216c",
+    "url": "/static/css/main.daae2b74.chunk.css"
+  },
+  {
+    "revision": "cccbea502128d375216c",
+    "url": "/static/js/main.cccbea50.chunk.js"
+  },
+  {
+    "revision": "e434db4d6ecd9c079668",
+    "url": "/static/js/1.e434db4d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dcb0c555526feaa494cf3e2acbccd781",
+    "url": "/index.html"
+  }
+];
